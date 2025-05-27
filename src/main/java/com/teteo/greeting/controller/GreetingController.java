faf6435f132019027886d800bc0e5f05b9177e70
Idinaldo Teteo <idinaldoteteo@gmail.com>
2025-05-27 22:42:20 +0100

    first commit

---
+++ src/main/java/com/teteo/greeting/controller/GreetingController.java
@@ -0,0 +1,17 @@
+package com.teteo.greeting.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+
+    @GetMapping
+    public ResponseEntity<String> greeting() {
+        return ResponseEntity.ok("Hello World. This application is UP.");
+    }
+
+}
